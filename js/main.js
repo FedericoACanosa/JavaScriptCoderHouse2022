9e47fe41bf75a73b371dfe992a0b9ebcdc52a9e2
FedericoCanosa <federicoadrielcanosa@gmail.com>
2022-06-28 11:20:32 -0300

    Actualizacion general/Añadido de cards

--- js/main.js
+++ js/main.js
@@ -1,187 +1,131 @@
-//Entrega Final I
+let carritoDeCompras = []
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+
+const botonTerminar = document.getElementById('terminar')
+const finCompra = document.getElementById('fin-compra')
+
+const contadorCarrito = document.getElementById('contadorCarrito');
+const precioTotal = document.getElementById('precioTotal');
+
+const selecPrecios = document.getElementById('selecPrecios')
+const buscador = document.getElementById('search')
 
 
-let catalogo = prompt(
-    '¡Elegí tu producto! \n-Elegi opciones \n- Trown Inc \n- Pine Tree \n- Ice \n- salir / siguiente opcion'
-);
 
-// Inician Arrays para pushear
+//hacemos el Filtro
+selecPrecios.addEventListener('change',()=>{
 
-let carrito = [];
-let precioenvio =[]
+    console.log(selecPrecios.value)
+    if(selecPrecios.value == 'all'){
+        mostrarProductos(stockProductos)
+    }else{
+        let arrayNuevo = stockProductos.filter(item => item.tipo == selecPrecios.value)//array nuevo
 
-//Terminan Arrays para pushear
-
-//Inicia Array de objetos
-
-const mercaderia = [
-    {
-    id: 1,
-    categoria: 'Gorras',
-    nombre: 'Trown Inc',
-    color: ['rojo', 'azul', 'verde'],
-    talle: 'u',
-    price: 2000 ,
-    },
-    {
-    id: 2,
-    categoria: 'Gorras',
-    nombre: 'Pine Tree',
-    color: ['rojo', 'azul', 'verde'],
-    talle: 'u',
-    price: 1555 ,
-    },
-
-    {
-    id: 3,
-    categoria: 'Gorras',
-    nombre: 'Ice',
-    stock: 1,
-    color: ['rojo', 'azul', 'verde'],
-    talle: 'u',
-    price: 1665 ,
-    },
-];
-
-// Termina Array de objetos
-
-// Inicia opciones de catalogo
-
-while (catalogo != 'salir') {
-    switch (catalogo) {
-    case 'Trown Inc':
-        let trowninc = prompt(
-            `El precio de  ${mercaderia[0].nombre} es de  ${mercaderia[0].price} ¿Quieres añadirlo al carrito? \n- Si \n- No`
-            );
-        if(trowninc=="si"){
-            carrito.push(mercaderia[0].price).parseFloat;
-            console.log(carrito); }
-        break;
-    case 'Pine Tree':
-        let pinetree = prompt(
-        `El precio de  ${mercaderia[1].nombre} es de  ${mercaderia[1].price} ¿Quieres añadirlo al carrito? \n- Si \n- No`
-        );
-        if(pinetree=="si"){
-            carrito.push(mercaderia[1].price).parseFloat;
-            console.log(carrito); }
-        break;
-    case 'Ice':
-        let ice= prompt(
-        `El precio de  ${mercaderia[2].nombre} es de  ${mercaderia[2].price} ¿Quieres añadirlo al carrito? \n- si \n- No`);
-        if(ice=="si"){
-        carrito.push(mercaderia[2].price).parseFloat;
-        console.log(carrito);
+        mostrarProductos(arrayNuevo)
     }
-        break;
-    default:
-        alert('La opcion no es valida, vuelva a ingresar');
-        break;
+})
+
+//EL STOCK
+let stockProductos = [
+	{id:1, nombre: "Body tigre", precio: 900, tipo: 1, desc: "Body de frizza estampado para bebé",  demora: "40min", img: './img/prod1.png'},
+	{id:2, nombre: "Jean wide leg", precio: 2300, tipo: 3, desc: "Jean elastizado con roturas y corte Oxford", demora: "3hs", img:'./img/prod2.png'},
+	{id:3, nombre: "Jean roturas", precio: 3700, tipo: 4, desc: "Jean elastizado con roturas, estampado y corte Oxford", demora: "4hs", img:'./img/prod3.png'},
+	{id:4, nombre: "Jean roturas/razgados", precio: 4200, tipo: 4, desc: "Jeans elastizados con rotaras y/o razgados", demora: "60min", img:'./img/prod4.png'},
+	{id:5, nombre: "Buzo anime", precio: 1200, tipo: 2, desc: "Buzo de algodón estampado.", demora: "45min", img:'./img/prod5.png'},
+	{id:6, nombre: "Buzo Smile", precio: 2100, tipo: 3, desc: "Buzo de frizza con estampado. ", demora: "3hs", img:'./img/prod6.png'},
+] 
+
+//Buscador o search
+
+mostrarProductos(stockProductos)
+
+//logica Ecommerce
+function mostrarProductos(array){
+
+    contenedorProductos.innerHTML = ""
+
+    for(const el of array) {
+
+        let div = document.createElement('div')
+        div.className = 'producto'
+        div.innerHTML = `<div class="card">
+                                <div class="card-image">
+                                    <img src="${el.img}">
+                                    <span class="card-titlesobre2">${el.nombre}</span>
+                                    <br></br>
+									<a id="boton${el.id}" href="#" class="btn btn-primary">Agregar<i class="bi bi-bag-heart"></i></a>
+                                    <br></br>
+                                </div>
+                                <div class="card-content">
+                                    <p>${el.desc}</p>
+                                    <p>Tipo: ${el.tipo}</p>
+                                    <p> $ ${el.precio}</p>
+                                </div>
+                            </div>`
+
+        contenedorProductos.appendChild(div)
+        
+        let btnAgregar = document.getElementById(`boton${el.id}`)
+        
+        btnAgregar.addEventListener('click',()=>{
+            agregarAlCarrito(el.id);
+        })
+
     }
-    catalogo = prompt(
-    '¡Elegí tu producto! \n-Elegi opciones \n- Trown Inc \n- Pine Tree \n- Ice \n- salir / siguiente opcion'
-    );
-}
-
-// Termina opciones de catalogo
-
-// Array para envío
-
-const mediosDeEnvio = [
-    {
-    id: 1,
-    nombre: 'Correo Argentino',
-    price: 1000 ,
-    },
-
-    {
-    id: 2,
-    nombre: 'Andreani',
-    price: 500 ,
-    },
-
-    {
-        id: 3,
-        nombre: 'Retiro por local',
-        price: 0 ,
-    },
-];
-
-// Finaliza Array para envío
-
-//Inicia menú para costo de envío
-
-let costoEnvio = prompt(
-    '¡Calcula tu envío! \n-Elegi alguna de las siguientes opciones \n- Correo Argentino \n- Andreani \n- Retiro por local \n- salir / siguiente opcion'
-);
-
-while (costoEnvio != 'salir') {
-    switch (costoEnvio) {
-    case 'Correo Argentino':
-        let CorreoArgentino= prompt(
-            `El precio de  ${mediosDeEnvio[0].nombre} es de  ${mediosDeEnvio[0].price} ¿Quieres añadirlo al carrito? \n- si \n- No`);
-            if(CorreoArgentino=="si"){
-            precioenvio.push(mediosDeEnvio[0].price).parseFloat;
-            console.log(precioenvio); }
-        break;
-    case 'Andreani':
-        let Andreani= prompt(
-            `El precio de  ${mediosDeEnvio[1].nombre} es de  ${mediosDeEnvio[1].price} ¿Quieres añadirlo al carrito? \n- si \n- No`);
-            if(Andreani=="si"){
-            precioenvio.push(mediosDeEnvio[1].price).parseFloat;
-            console.log(precioenvio); }
-        break;
-    case 'Retiro por local':
-        let retiroPorLocal= prompt(
-            `El precio de  ${mediosDeEnvio[2].nombre} es de  ${mediosDeEnvio[2].price} ¿Quieres añadirlo al carrito? \n- si \n- No`);
-            if(retiroPorLocal=="si"){
-            precioenvio.push(mediosDeEnvio[2].price).parseFloat;
-            console.log(precioenvio); }
-        break;
-    default:
-        alert('La opcion no es valida, vuelva a ingresar');
-        break;
-    }
-    costoEnvio = prompt(
-    'Elegi alguna de las siguientes opciones \n- Correo Argentino \n- Andreani \n- Retiro por local \n- salir / siguiente opcion'
-    );
-}
-
-// Termina menú para costo de envío
-
-// Inicia bloque de determinación de resultados.
-let resultado = parseFloat(carrito) + parseFloat(precioenvio) 
-
-let impresion= document.getElementById('Container-principal')
-
-function mostrar(mensaje) {
-
-    const unaCuota= document.createElement('button'); 
-    unaCuota.type = 'button'; 
-    unaCuota.innerText = 'Pagar en una cuota'; 
-    document.body.appendChild(unaCuota);
-    unaCuota.addEventListener("click", function(){ console.log(`pago final de  ${parseFloat(resultado).toFixed(2)}`) });
-
-    const tresCuotas= document.createElement('button'); 
-    tresCuotas.type = 'button'; 
-    tresCuotas.innerText = 'Pagar en 3 cuotas'; 
-    document.body.appendChild(tresCuotas);
-    tresCuotas.addEventListener("click", function(){ console.log(`pago final de  ${parseFloat(resultado/3).toFixed(2)}`) });
-
-    const seisCuotas= document.createElement('button'); 
-    seisCuotas.type = 'button'; 
-    seisCuotas.innerText = 'Pagar en 6 cuotas'; 
-    document.body.appendChild(seisCuotas);
-    seisCuotas.addEventListener("click", function(){ console.log(`pago final de  ${parseFloat(resultado/6).toFixed(2)}`) });
-
-    impresion.innerHTML=`El precio total a pagar es de ${mensaje }`;
-
+    
+   
 }
 
 
-mostrar(resultado);
 
-// Termina bloque de determinación de resultados.
+function agregarAlCarrito(id) {
+    let yaExiste = carritoDeCompras.find(elemento => elemento.id == id)
 
-// DOM, procedo a capturar mediante un prompt una salida al html
+    if(yaExiste){
+        yaExiste.cantidad = yaExiste.cantidad + 1
+        document.getElementById(`cantidad${yaExiste.id}`).innerHTML = `<p id="cantidad${yaExiste.id}">cantidad: ${yaExiste.cantidad}</p>`
+        actualizarCarrito()
+    }else{
+        let productoAgregar = stockProductos.find(ele => ele.id === id)
+        productoAgregar.cantidad = 1 
+   carritoDeCompras.push(productoAgregar)
+   actualizarCarrito()
+   mostrarCarrito(productoAgregar) 
+    }
+}
 
 
+
+function mostrarCarrito(productoAgregar) {
+
+    let div = document.createElement('div')
+    div.classList.add('productoEnCarrito')
+    div.innerHTML =`<p>${productoAgregar.nombre}</p>
+                <p>Precio: $${productoAgregar.precio}</p>
+                <p id="cantidad${productoAgregar.id}">cantidad: ${productoAgregar.cantidad}</p>
+                <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="bi bi-trash"></i></button>`
+    contenedorCarrito.appendChild(div)
+
+    let btnEliminar= document.getElementById(`eliminar${productoAgregar.id}`)
+    btnEliminar.addEventListener('click',()=>{
+        if(productoAgregar.cantidad == 1){
+            btnEliminar.parentElement.remove()
+            carritoDeCompras = carritoDeCompras.filter(item => item.id !== productoAgregar.id)
+            actualizarCarrito()
+        }else{
+            productoAgregar.cantidad = productoAgregar.cantidad - 1
+        document.getElementById(`cantidad${productoAgregar.id}`).innerHTML = `<p id="cantidad${productoAgregar.id}">cantidad: ${productoAgregar.cantidad}</p>`
+        actualizarCarrito()
+        }
+    
+
+    })
+}
+
+
+function  actualizarCarrito (){
+    contadorCarrito.innerText = carritoDeCompras.reduce((acc,el)=> acc + el.cantidad, 0)
+   precioTotal.innerText = carritoDeCompras.reduce((acc,el)=> acc + (el.precio * el.cantidad) , 0)                                                            
+}
